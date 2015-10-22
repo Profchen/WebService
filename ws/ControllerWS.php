697a7ae45ee92a0c971331e82e89b2b0aca509a3
ChriStauffer <shazbotown@gmail.com>
2015-10-22 08:27:32 +0200

    modifs db, controleur et user

---
+++ ws/ControllerWS.php
@@ -0,0 +1,29 @@
+<?php
+
+	include_once('Access.php');
+
+	const PARAM_WS = 'ws';
+	const PATH_WEBSERVICES = 'webservices';
+
+	// We verify all needed parameters.
+	if(!isset($_GET[PARAM_WS]))
+		Access::ThrowAccessDenied();
+
+	// We gets the informations of the desired service.
+	$serviceName = ucfirst(strtolower($_GET['ws']).'WS');
+	$servicePath = PATH_WEBSERVICES.'\\'.$serviceName.'.php';
+
+	// If the service doesn't exist, we stop the request.
+	if (!file_exists($servicePath))
+		Access::ThrowAccessDenied();
+
+	// We create and execute the service.
+	require_once($servicePath);
+	$service = new $serviceName();
+	$result = $service->DoGet();
+
+	// At the end, we return the result.
+	if ($result !== null)
+		echo json_encode($result);
+	
+?>
--- ws/WS_User.php
+++ ws/WS_User.php
@@ -1,44 +1,126 @@
 <?php
 
-require_once '/IWebServiciable.php';
-
-class WS_User implements IWebServiciable {
-    
-    public $requestParams;
-    
-    function __construct($requestParams) {
-        $this->requestParams = $requestParams;
-    }
-
-    public function doDelete() {
-        
-    }
-
-    public function doGet() {
+	require_once 'IWebServiciable.php';
+	require_once 'database/db_connect.php';
+	
+	session_start();
+	const PARAM_ACTION = 'action';
+	const REGISTER_ADMIN = 'register';
+	const LOGOUT_USER = 'logout';
+	const GET_VERIF_USER = 'login';
+	const REMOVE_USER = 'remove';
+	const MODIFY_USER = 'modify';
+	// const SQL_GET_ALL_USER = "SELECT idAdmin, login, password FROM admin";
+	// const SQL_GET_VERIF_USER = "SELECT password FROM admin WHERE login=".$login." AND password=".$password"";
+	
+	class WS_USER implements IWebServiciable
+	{
 		
-    }
+		public function DoGet()
+		{
+			if (!isset($_GET[PARAM_ACTION]))
+				Access::ThrowAccessDenied();
 
-    public function doPost() {
-        
-    }
+			switch ($_GET[PARAM_ACTION])
+			{
+				case GET_VERIF_USER:
+					return $this->getVerifUser();
+				
+				case GET_USERS:
+					return $this->getAllUser();
+					
+				case LOGOUT_USER:
+					return $this->logout();
+					
+				case REGISTER_ADMIN:
+					return $this->register();
+					
+				case REMOVE_USER:
+					return $this->remove();
+				
+				case MODIFY_USER:
+					return $this->modify();
 
-    public function doPut() {
-        
-    }
+				default:
+					Access::ThrowAccessDenied();
+					break;
+			}
+		}
+		
+		private function getVerifUser(){
+			$login = $_GET['login'];
+			$password = $_GET['password'];
+			
+			
+			$sql = "SELECT password FROM admin WHERE login='".$login."' AND password='".$password."'";
+			
+			MySQL::Execute($sql);
+			$verif = MySQL::GetResult()->fetchAll();
+			
+			if(count($verif) !== 0){
+				$_SESSION['Logged'] = 1;
+				// var_dump($_SESSION['Logged']);
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		
+		private function logout(){
+			
+			if (isset($_SESSION['Logged']) && $_SESSION['Logged'] !== 0){
+				$_SESSION['Logged'] = 0; 
+				return true;
+			}	
+			return false;
+			
+		}
+		
+		private function getAllUser(){
+			MySQL::Execute(SQL_GET_ALL_USER);
 
-    public function doRequest() {
-        
-    }
+			return MySQL::GetResult()->fetchAll();
+		}
+		
+		private function register(){
+			
+			if (!isset($_GET['login']) || !isset($_GET['password']))
+				Helper::ThrowAccessDenied();
+			
+			MySQL::Execute("INSERT INTO admin(login, password) VALUES ('".$_GET['login']."','".$_GET['password']."')");
+		}
+		
+		private function remove(){
+			if (!isset($_GET['idAdmin']))
+				Helper::ThrowAccessDenied();
+			MySQL::Execute('DELETE FROM admin WHERE idAdmin='.$_GET['idAdmin']);
+			
+		}
+		
+		private function modify(){
+			if (!isset($_GET['idAdmin']) || !isset($_GET['login']) || !isset($_GET['password']))
+				Helper::ThrowAccessDenied();
+			
+			MySQL::Execute("UPDATE admin SET login='".$_GET['login']."', password='".$_GET['password']."' WHERE idAdmin=".$_GET['idAdmin']);
+			
+		}
+		
+		public function DoPost()
+		{
+			Helper::ThrowAccessDenied();
+		}
+		
+		public function DoPut()
+		{
+			Helper::ThrowAccessDenied();
+		}
 
-    public function setParameters() {
-        
-    }
-    
-    public function doNeedAuth() {
-        return true;
-    }
+		public function DoDelete()
+		{
+			Helper::ThrowAccessDenied(); 
+		}
+	}
 
-    
-} 
-
+	
 ?>
