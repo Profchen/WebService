a1f06492c3c0ed44f53e0a1a3b82b4d79b4df713
clement chene <clementchene59500@gmail.com>
2015-10-22 09:08:29 +0200

    petite modif
    Kappa

--- ws/WS_File.php
+++ ws/WS_File.php
@@ -1,8 +1,16 @@
 <?php
 
-require_once '/IWebServiciable.php';
+        require_once '/IWebServiciable.php';
+	require_once('database/db_connect.php');
 
-const 
+        const ADD_EPISODE =('add');
+        const REMOVE_EPISODE =('remove');
+        const PARAM_ACTION =('action');
+        const PARAM_EPISODE_ID =('idEpisode');
+        const PARAM_EPISODE =('episode');
+        const SQL_INSERT_EPISODE = "INSERT INTO EPISODES(Name, Url, id) VALUES ('%s','%s','%d')";
+	const SQL_UPDATE_EPISODE_TIMER = 'UPDATE EPISODES SET Name="%s",Url="%s" WHERE idQuestion="%d"';
+        
 
 class WS_File implements IWebServiciable {
     
